c72b05ce98838875ebb6e7e317456018ff95008b
eoussama <oussama.essamadi@gmail.com>
2024-07-25 13:24:59 +0100

    feat: card type

---
+++ include/cardtype.h
@@ -0,0 +1,56 @@
+#ifndef CARDTYPE_H
+#define CARDTYPE_H
+
+#include <string>
+
+enum class CardType
+{
+  COMMON_BRONZE = 1,
+  COMMON_SILVER,
+  COMMON_GOLD,
+  RARE_BRONZE,
+  RARE_SILVER,
+  RARE_GOLD,
+  IF_BRONZE,
+  IF_SILVER,
+  IF_GOLD,
+  FC_BRONZE,
+  FC_SILVER,
+  FC_GOLD,
+  MOTM,
+  PL_POTM,
+  BL_POTM,
+  FUTTIES,
+  FUTTIESW,
+  TOTY,
+  TOTY_N,
+  EL,
+  EL_MOTM,
+  EL_LIVE,
+  EL_SBC,
+  EL_TOTT,
+  COMMON_UCL,
+  RARE_UCL,
+  UCL_MOTM,
+  UCL_LIVE,
+  UCL_SBC,
+  UCL_TOTT,
+  FSR,
+  FS,
+  FSN,
+  PP,
+  CB,
+  RB,
+  HERO,
+  AW,
+  FB,
+  HEADLINERS,
+  CC,
+  SBC,
+  SBCP,
+  LEGEND
+};
+
+std::string getCardName(const CardType &card);
+
+#endif
