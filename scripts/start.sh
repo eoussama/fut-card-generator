0b70a2f542db4d27a050fed10609c64a517d1062
eoussama <oussama.essamadi@gmail.com>
2024-08-04 00:14:00 +0100

    feat: fixed volumes, #6

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,10 +9,5 @@ image="$username/$project:$tag"
 
 docker build -f ./docker/Dockerfile -t $image .
 docker run -it --rm \
-  -v "$(pwd)/src":/fut-card-generator/src \
-  -v "$(pwd)/.env":/fut-card-generator/.env \
-  -v "$(pwd)/build":/fut-card-generator/build \
-  -v "$(pwd)/assets":/fut-card-generator/assets \
-  -v "$(pwd)/include":/fut-card-generator/include \
-  -v "$(pwd)/scripts":/fut-card-generator/scripts \
+  -v "$(pwd)":/fut-card-generator \
   $image
