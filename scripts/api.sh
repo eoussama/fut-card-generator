9a6c5c7f9a7f9debbe51235946923d4cc6a008cd
eoussama <oussama.essamadi@gmail.com>
2024-08-04 14:51:46 +0100

    feat: api docker image, #8

---
+++ scripts/api.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source .env
+
+tag=$GLOBAL_VERSION
+project="fut-card-generator"
+username="eoussama"
+image="$username/$project:$tag-api"
+
+docker build -f ./docker/Dockerfile.api -t $image .
+docker run -it --rm \
+  -p 5000:5000 \
+  -v "$(pwd)":/fut-card-generator \
+  $image
